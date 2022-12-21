b3df54f77358b663d0a630dab5581a9047b7b4f9
AnnaKim <annakim@inbox.ru>
2022-12-21 18:12:56 +0300

    task_8_modified

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -2,13 +2,11 @@
 //and shows all the ordinary numbers from 1 to number
 
 Console.WriteLine("Input number");
-int number = int.Parse (Console.ReadLine()!);
+int number = int.Parse(Console.ReadLine()!);
 int  NumberOrd= 2;
 
 while (NumberOrd <= number)
     {
-        NumberOrd++; 
+        Console.WriteLine(NumberOrd+"");
+        NumberOrd=NumberOrd+2; 
     }
-    if (NumberOrd%2==0)
- 
- Console.WriteLine(NumberOrd+"");
