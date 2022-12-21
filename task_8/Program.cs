5dc89bf08f0e47f975be3e31e766e4d93b7731df
AnnaKim <annakim@inbox.ru>
2022-12-21 18:00:15 +0300

    task_8_completed

---
+++ task_8/Program.cs
@@ -0,0 +1,14 @@
+﻿//task_8 program takes a number 
+//and shows all the ordinary numbers from 1 to number
+
+Console.WriteLine("Input number");
+int number = int.Parse (Console.ReadLine()!);
+int  NumberOrd= 2;
+
+while (NumberOrd <= number)
+    {
+        NumberOrd++; 
+    }
+    if (NumberOrd%2==0)
+ 
+ Console.WriteLine(NumberOrd+"");
