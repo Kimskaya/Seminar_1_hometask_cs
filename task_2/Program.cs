d95e2e504360a810d0b6f63b8b45a0731ac9c2fa
AnnaKim <annakim@inbox.ru>
2022-12-21 18:20:46 +0300

    task_2_completed

---
+++ task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// task_2. Program that takes 2 numbers and shows the biggest
+Console.WriteLine("Input number");
+int numberX = int.Parse  (Console.ReadLine()!);
+
+Console.WriteLine("Input number");
+int numberY = int.Parse (Console.ReadLine()!);
+if (numberX>numberY)
+
+Console.WriteLine(numberX+ " is max");
+else
+Console.WriteLine(numberY+" is max");
