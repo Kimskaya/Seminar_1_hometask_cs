9130a944104b57f9dc104dd54a7c1c7802ddb84d
AnnaKim <annakim@inbox.ru>
2022-12-21 18:15:13 +0300

    task_6_completed

---
+++ task_6/Program.cs
@@ -0,0 +1,9 @@
+﻿// task_6 program takes a number and outputs 
+//if the number is ordinary
+
+Console.WriteLine("Input number");
+int numberA = int.Parse  (Console.ReadLine()!);
+if (numberA%2==0)
+Console.WriteLine(numberA+" is an ordinary number");
+else 
+Console.WriteLine(numberA+ " is an odd number");
